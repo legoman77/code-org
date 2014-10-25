1aa7461eed70e2f2ba371e2f454971357d4daf86
legoman77 <rplegos@gmail.com>
2014-10-25 10:20:55 -0400

    level 28

---
+++ s1level28.py
@@ -0,0 +1,23 @@
+'''for (var count = 0; count < 3; count++) {
+  moveForward(100);
+  turnRight(120);
+}
+for (var count2 = 0; count2 < 4; count2++) {
+  moveForward(100);
+  turnRight(90);
+}
+'''
+
+import turtle
+
+bob = turtle.Turtle()
+bob.pensize(7)
+bob.pencolor('red')
+
+for count in range(3):
+    bob.forward(100)
+    bob.right(120)
+
+for count in range(4):
+    bob.forward(100)
+    bob.right(90)
